66a65fda03be658e56de462fe2a43c38d0fe6419
Hibana <pagliaccizeus@gmail.com>
2021-03-25 13:14:31 +0100

    first version of Tic Tac Toe

--- main.js
+++ main.js
@@ -1,68 +1,160 @@
-let inputdata = document.querySelector("#dataEntry");
+let rowArr = new Array(3);
+let countClick = 0;
 
-//on keyword input code
-document.addEventListener("keydown",(event)=> {
-    if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 97 && event.keyCode <= 111) || event.keyCode === 8 || event.keyCode === 46) {
-        document.querySelector("#dataEntry").focus();
-    } 
-    try {
-        if (event.key === "Enter") {
-            let zadanie = inputdata.value;
-            // console.log(eval(zadanie));
-            inputdata.value = eval(zadanie)
-        }   
-        if (event.key === "Delete") {
-            inputdata.value = "";
-        }
-    } catch (err) {
-        inputdata.value = "wrong Input"
+
+//creating a playing field
+function createGame() {
+    //create game arr
+    for (let i =0;i<rowArr.length;i++) {
+        rowArr[i] = new Array(3);
     }
-})
-//on button input realize
-document.querySelector(".buttonsCalculator").addEventListener("click", function(e) {
-    try {
-        if (event.key === "Enter" ){
-            let zadanie = inputdata.value;
-            inputdata.value = eval(zadanie)
+    //creating a playing field
+    let html = "";
+    for(let i=0;i<rowArr.length;i++) {
+        for (let j=0;j<rowArr[i].length;j++) {
+            rowArr[i][j] = "";
+            html += `<button value="${i +" "+ j}"  class="btn_click">${rowArr[i][j]}</button>`
+            document.querySelector("#block_game").innerHTML = html;
         }
-        if (e.target.classList.contains("btn_value")) {
-            // console.log(e.target.innerText);
-            inputdata.value += e.target.innerText;
-        }
-    } catch (err) {
-        inputdata.value = "wrong Input"
-    }
-})
-
-
-document.querySelector(".buttons_operations").addEventListener("click", function(e) {
-    if (e.target.classList.contains("btn_operation")) {
-        // console.log(e.target.innerText);
-        inputdata.value += e.target.innerText;
-    }
-})
-
-//get result of operation
-document.querySelector(".btn_result").onclick = ()=> {
-    // console.log(inputdata.value)
-    try {
-        if (inputdata.value === "") {
-
-        } else {
-            let zadanie = inputdata.value;
-            inputdata.value = eval(zadanie)
-        }
-    } catch(err) {
-        inputdata.value = "Wrong Input";
     }
 }
 
-//backspace
-document.querySelector(".btn_backspace").onclick = ()=> {
-    inputdata.value = inputdata.value.substring(0,inputdata.value.length-1);
+
+function Game () {
+    document.querySelector("#block_game").addEventListener("click",function(e) {
+        if (e.target.classList.contains("btn_click")) {
+            if (countClick % 2 === 0) {
+                    let position = e.target.value;
+                let arr = position.split(" ");
+                rowArr[arr[0]][arr[1]] = "0"
+
+                if (e.target.innerText === "") {
+                    countClick++;
+                    e.target.innerText = "0";
+                } else {
+                    alert("you already input on button");
+                }
+
+                check("0")
+            } else if (countClick % 2 !== 0) {
+                let position = e.target.value;
+                    let arr = position.split(" ");
+                    rowArr[arr[0]][arr[1]] = "X"
+                    // console.log(rowArr)
+        
+                    if (e.target.innerText === "") {
+                        countClick++;
+                        e.target.innerText = "X";
+                    } else {
+                        alert("you already input on button");
+                    }
+
+                    check("X");
+            }
+        } 
+    })  
 }
-//delete
-document.querySelector(".btn_delete").onclick = ()=> {
-    inputdata.value = "";
-} 
+
+function check(val) {
+    let count =0;
+    for (let i=0;i<rowArr.length;i++) {
+        for (let j=0;j<rowArr[i].length;j++) {
+            if (rowArr[0][j] === val || rowArr[1][j] === val || rowArr[i][0] === val || rowArr[2][j] === val || rowArr[i][1] === val || rowArr[i][2] === val) {
+                count++;  
+                // console.log(count);
+                if (count/3 == 3) {
+                    return alert(`You win ${val}`);
+                } 
+             
+            } else if (i==j && rowArr[i][j] === val) {
+                count++;  
+
+                if (count/3 == 3) {
+                    return alert(`You win ${val}`);
+                }
+            }
+        }
+    }
+}
+
+
+document.querySelector("#btn_startNewGame").onclick = ()=> {
+    createGame();
+    countClick = 0
+}
+
+createGame();
+Game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Rework Coming Soon:
+// function clickPlayer() {
+//     document.querySelector("#block_game").addEventListener("click",function(e) {
+//         if (e.target.classList.contains("btn_click")) {
+//             let position = e.target.value;
+//             // console.log(e.target.value);
+//             let arr = position.split(" ");
+//             // console.log(e.target.innerText);
+//             rowArr[arr[0]][arr[1]] = "0"
+//             // console.log(rowArr)
+//             // e.target.innerText === "" ? e.target.innerText = "0" : alert("you already input on button"); 
+
+//             if (e.target.innerText === "") {
+//                 e.target.innerText = "0";
+//                 countClick++;
+//             } else {
+//                 alert("you already input on button");
+//             }
+//             // console.log(rowArr);
+//         } 
+        
+//         // console.log(countClick);
+//     })  
+// }
+
+
+// function clickBot() {
+//     document.querySelector("#block_game").addEventListener("click",function(e) {
+//         if (e.target.classList.contains("btn_click")) {
+//             let position = e.target.value;
+//             // console.log(e.target.value);
+//             let arr = position.split(" ");
+//             // console.log(e.target.innerText);
+//             rowArr[arr[0]][arr[1]] = "Х"
+//             console.log(rowArr)
+//             e.target.innerText === "" ? e.target.innerText = "Х" : alert("you already input on button"); 
+
+//             if (e.target.innerText === "") {
+//                 e.target.innerText = "X";
+//                 countClick++;
+//             } else {
+//                 alert("you already input on button");
+//             }
+
+//             // console.log(rowArr);
+//         }
+//         // console.log(countClick);
+//         // countClick++;
+//         // console.log(countClick);
+//     })
+   
+// }
+
 
